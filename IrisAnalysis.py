84434ec69e14dbadee65ba4138b6270938bb14b8
AniketPant02 <pant.aniket@gmail.com>
2017-07-05 20:17:27 -0500

    Working on Iris KNN

---
+++ IrisAnalysis.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 19:31:06 2017
+
+@author: Aniket Pant
+"""
+
+import numpy as np
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+
+from sklearn.neighbors import KNeighborsClassifier
+
+# Generating Instance of KNN model
+knn = KNeighborsClassifier(n_neighbors=1)
+
+# Fitting data to model
+knn.fit(X, y)
+
+knn.predict([[3,5,4,2]])
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Machine-Learning
-Machine-Learning
+Various Machine Learning Projects
