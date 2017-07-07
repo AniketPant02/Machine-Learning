c6f750ce704f59cc705ca19a0191b4e72da78238
AniketPant02 <pant.aniket@gmail.com>
2017-07-07 18:11:40 -0500

    Removed Unneccesary Code
    Removed Unneccesary Code

--- Titanic Kaggle/titanickaggle.py
+++ Titanic Kaggle/titanickaggle.py
@@ -15,21 +15,16 @@ import seaborn as sns
 trainData = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Titanic Kaggle\\data\\train.csv', index_col=0)
 testData = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Titanic Kaggle\\data\\test.csv', index_col=0)
 
-# Reassigning Values
-trainData.replace('male', 1)
-trainData.replace('female', 0)
+# Reassigning Sex Column Values -> 0 = Female, 1 = Male
+trainData['Sex'].replace('male', 1, inplace=True)
+trainData['Sex'].replace('female', 0, inplace=True)
+# Reassigning Embarked Column Values -> 0 = C, 1 = S, 2 = Q
+trainData['Embarked'].replace('C', 0, inplace=True)
+trainData['Embarked'].replace('S', 1, inplace=True)
+trainData['Embarked'].replace('Q', 2, inplace=True)
 
-
-X = trainData.drop(['Survived', 'Name', 'Embarked', 'Ticket', 'Cabin', 'Sex', 'Age'], axis=1)
+X = trainData.drop(['Survived', 'Name', 'Embarked', 'Ticket', 'Cabin', 'Age'], axis=1)
 y = trainData['Survived']
-
-
-'''
-sns.pairplot(trainData, x_vars=['Pclass', 'SibSp', 'Parch', 'Fare'], y_vars=['Survived'], size=7, aspect=0.7,kind='reg')
-sns.pointplot(x="Pclass", y="Survived", hue="Sex", data=trainData,
-              palette={"male": "blue", "female": "pink"},
-              markers=["*", "o"], linestyles=["-", "--"]);
-'''
 
 # Importing KNN
 from sklearn.neighbors import KNeighborsClassifier
@@ -45,34 +40,3 @@ from sklearn.cross_validation import cross_val_score
 
 scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy')
 print (scores.mean())
-
-'''
-k_range = list(range(1,31))
-k_scores = []
-for k in k_range:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy')
-    k_scores.append(scores.mean())
-print(k_scores)
-
-import matplotlib.pyplot as plt
-plt.plot(k_range, k_scores)
-plt.xlabel("Value of K")
-plt.ylabel("Accuracy")
-plt.show()
-'''
-'''
-# Importing Train_Test_Split to generate training and testing set to check coefficients
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
-
-
-# Importing LinearReg Model in SciKit
-from sklearn.linear_model import LinearRegression
-
-# Generating Instance
-linreg = LinearRegression()
-
-# fit the model to the training data
-linreg.fit(X_train, y_train)
-'''
