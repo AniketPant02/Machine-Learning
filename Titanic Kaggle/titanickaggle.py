9163d94b068bccc4e686ebaee98b93443a4f7381
AniketPant02 <pant.aniket@gmail.com>
2017-07-07 16:38:36 -0500

    Replacing Column Values

--- Titanic Kaggle/titanickaggle.py
+++ Titanic Kaggle/titanickaggle.py
@@ -10,30 +10,57 @@ Created on Thu Jul  6 19:29:09 2017
 # Importing General Modules
 import pandas as pd
 import seaborn as sns
-import numpy as np
 
 # Extracting data from CSV files
 trainData = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Titanic Kaggle\\data\\train.csv', index_col=0)
 testData = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Titanic Kaggle\\data\\test.csv', index_col=0)
 
+# Reassigning Values
+trainData.replace('male', 1)
+trainData.replace('female', 0)
+
+
 X = trainData.drop(['Survived', 'Name', 'Embarked', 'Ticket', 'Cabin', 'Sex', 'Age'], axis=1)
 y = trainData['Survived']
 
+
+'''
 sns.pairplot(trainData, x_vars=['Pclass', 'SibSp', 'Parch', 'Fare'], y_vars=['Survived'], size=7, aspect=0.7,kind='reg')
+sns.pointplot(x="Pclass", y="Survived", hue="Sex", data=trainData,
+              palette={"male": "blue", "female": "pink"},
+              markers=["*", "o"], linestyles=["-", "--"]);
+'''
 
 # Importing KNN
 from sklearn.neighbors import KNeighborsClassifier
 
 # Generating KNN Instance
-knn = KNeighborsClassifier(n_neighbors=1) # K-value = 1 (Optimize Later)
+knn = KNeighborsClassifier(n_neighbors=9)
 
 # Fitting data to model
 knn.fit(X, y)
-z = knn.predict([[3,1,0,7.25], [3,1,0,71.3]])
-print("Printing Predictions")
-print(z)
-print("-----------------------------------------")
 
+# Searching for an optimal value of K
+from sklearn.cross_validation import cross_val_score
+
+scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy')
+print (scores.mean())
+
+'''
+k_range = list(range(1,31))
+k_scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy')
+    k_scores.append(scores.mean())
+print(k_scores)
+
+import matplotlib.pyplot as plt
+plt.plot(k_range, k_scores)
+plt.xlabel("Value of K")
+plt.ylabel("Accuracy")
+plt.show()
+'''
 '''
 # Importing Train_Test_Split to generate training and testing set to check coefficients
 from sklearn.cross_validation import train_test_split
