27e2a7bc92a2c86739f8f92cd41cca74d430bc6b
AniketPant02 <pant.aniket@gmail.com>
2017-07-06 20:27:25 -0500

    Working on graphs

--- titanickaggle.py
+++ titanickaggle.py
@@ -9,8 +9,15 @@ Created on Thu Jul  6 19:29:09 2017
 
 # Importing General Modules
 import pandas as pd
-import numpy as np
+import seaborn as sns
 
 # Extracting data from CSV format
-data = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Titanic Kaggle\\data\\train.csv')
-print (data.head())
+trainData = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Titanic Kaggle\\data\\train.csv')
+testData = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Titanic Kaggle\\data\\test.csv')
+
+# Dropping unnecessary columns not needed for data analysis
+trainData = trainData.drop(['PassengerId','Name','Ticket'], axis=1)
+testData    = testData.drop(['Name','Ticket'], axis=1)
+
+# Introduce LinRegression here to see what affects data most (or use all fields?)
+sns.pairplot(trainData, x_vars=['Sex', 'Age', 'SibSp', 'Parch', 'Fare'], y_vars='Survived', size=7, aspect=0.7,kind='reg')
