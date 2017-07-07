84e41a5405d54b7862b9ede58f20bfa0a849b7e5
AniketPant02 <pant.aniket@gmail.com>
2017-07-06 19:56:13 -0500

    Added datasets and import data into script using Pandas
    Added datasets and import data into script using Pandas

---
+++ titanickaggle.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  6 19:29:09 2017
+
+@author: Aniket Pant
+"""
+
+# Titanic Dataset Analysis, ML Approach
+
+# Importing General Modules
+import pandas as pd
+import numpy as np
+
+# Extracting data from CSV format
+data = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Titanic Kaggle\\data\\train.csv')
+print (data.head())
