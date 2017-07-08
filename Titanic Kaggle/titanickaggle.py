4ead9a0a0ede64f2958672fa5913bfb71486130b
AniketPant02 <pant.aniket@gmail.com>
2017-07-08 14:06:31 -0500

    Finished model. 76% Accuracy, Cross Val
    Finished model. 76% Accuracy, Cross Val.
    ToDo: Submit scores to Kaggle

--- Titanic Kaggle/titanickaggle.py
+++ Titanic Kaggle/titanickaggle.py
@@ -6,7 +6,7 @@ Created on Thu Jul  6 19:29:09 2017
 """
 
 # Titanic Dataset Analysis, ML Approach
-
+    
 # Importing General Modules
 import pandas as pd
 import seaborn as sns
@@ -14,7 +14,8 @@ import seaborn as sns
 # Extracting data from CSV files
 trainData = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Titanic Kaggle\\data\\train.csv', index_col=0)
 testData = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Titanic Kaggle\\data\\test.csv', index_col=0)
-
+# Adding Survived Columns to Test Data CSV
+# testData = testData.assign(Survived=pd.Series(np.random.randn(testDataLength)).values)
 # Reassigning Sex Column Values -> 0 = Female, 1 = Male
 trainData['Sex'].replace('male', 1, inplace=True)
 trainData['Sex'].replace('female', 0, inplace=True)
@@ -22,9 +23,18 @@ trainData['Sex'].replace('female', 0, inplace=True)
 trainData['Embarked'].replace('C', 0, inplace=True)
 trainData['Embarked'].replace('S', 1, inplace=True)
 trainData['Embarked'].replace('Q', 2, inplace=True)
+# Reassigning Column Values in Test CSV
+testData['Sex'].replace('male', 1, inplace=True)
+testData['Sex'].replace('female', 0, inplace=True)
+testData['Embarked'].replace('C', 0, inplace=True)
+testData['Embarked'].replace('S', 1, inplace=True)
+testData['Embarked'].replace('Q', 2, inplace=True)
 
-X = trainData.drop(['Survived', 'Name', 'Embarked', 'Ticket', 'Cabin', 'Age'], axis=1)
-y = trainData['Survived']
+
+X_train = trainData.drop(['Survived', 'Name', 'Embarked', 'Ticket', 'Cabin', 'Age'], axis=1)
+y_train = trainData['Survived']
+X_test = testData.drop(['Survived', 'Name', 'Embarked', 'Ticket', 'Cabin', 'Age'], axis=1)
+y_test = testData['Survived']
 
 # Importing KNN
 from sklearn.neighbors import KNeighborsClassifier
@@ -33,10 +43,11 @@ from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors=9)
 
 # Fitting data to model
-knn.fit(X, y)
+knn.fit(X_train, y_train)
 
-# Searching for an optimal value of K
-from sklearn.cross_validation import cross_val_score
+submission = pd.DataFrame({
+        "PassengerId": testData.index,
+        "Survived": knn.predict(X_test, y_test)
+    })
 
-scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy')
-print (scores.mean())
+submission.to_csv('test.csv', index=False)
