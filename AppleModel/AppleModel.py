18efca69bea18b1cbfc9be7c9655f82f804c892c
AniketPant02 <pant.aniket@gmail.com>
2017-07-18 19:32:03 -0500

    Added BCP Data and Started Script

--- AppleModel/AppleModel.py
+++ AppleModel/AppleModel.py
@@ -18,4 +18,12 @@ feature_cols = ['Open', 'High', 'Low', 'Volume']
 X = stockFrame.loc[:, feature_cols]
 y = stockFrame.Close
 
-sns.pairplot(stockFrame, x_vars=['Close'], y_vars=['Open', 'High', 'Low', 'Volume'], size=7, aspect=0.7, kind='reg')
+# sns.pairplot(stockFrame, x_vars=['Close'], y_vars=['Open', 'High', 'Low', 'Volume'], size=7, aspect=0.7, kind='reg')
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression(fit_intercept=False)
+model.fit(X,y)
+
+from sklearn.cross_validation import cross_val_score
+scores = cross_val_score(model, X, y, cv=10, scoring='accuracy')
+print(scores)
---
+++ Breast Cancer Proteomes/breastCancerAnalysis.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 14:24:40 2017
+
+@author: Aniket Pant
+"""
+
+# Breast Cancer Subclass Classifier - iTRAQ Data Supplicant
+import pandas as pd
+import matplotlib.pyplot as plt
+
+clinicalData = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Breast Cancer Proteomes\\breastcancerproteomes\\clinical_data_breast_cancer.csv')
+pam50proteinData = pd.read_csv("C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Breast Cancer Proteomes\\breastcancerproteomes\\PAM50_proteins.csv")
+cancerProteomeData = pd.read_csv("C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Breast Cancer Proteomes\\breastcancerproteomes\\77_cancer_proteomes_CPTAC_itraq.csv")
