4670a58060850631c511088d2e232d0ecd79819a
AniketPant02 <pant.aniket@gmail.com>
2017-07-19 14:01:37 -0500

    Background Research Files
    Added AJCC Staging information

--- Breast Cancer Proteomes/breastCancerAnalysis.py
+++ Breast Cancer Proteomes/breastCancerAnalysis.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jul 18 14:24:40 2017
-
+ 
 @author: Aniket Pant
 """
 
@@ -12,3 +12,4 @@ import matplotlib.pyplot as plt
 clinicalData = pd.read_csv('C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Breast Cancer Proteomes\\breastcancerproteomes\\clinical_data_breast_cancer.csv')
 pam50proteinData = pd.read_csv("C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Breast Cancer Proteomes\\breastcancerproteomes\\PAM50_proteins.csv")
 cancerProteomeData = pd.read_csv("C:\\Users\\Aniket Pant\\Documents\\GitHub\\Machine-Learning\\Breast Cancer Proteomes\\breastcancerproteomes\\77_cancer_proteomes_CPTAC_itraq.csv")
+
