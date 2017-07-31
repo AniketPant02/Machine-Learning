dbf3787e66344ede8eefdead279d5fa0952e68ef
AniketPant02 <pant.aniket@gmail.com>
2017-07-31 16:07:00 -0500

    Attempting to Import HDF5 Protein Kinase Files and Working On Data Analysis in IpNB
    Attempting to Import HDF5 Protein Kinase Files and Working On Data Analysis in IpNB

--- dfCreation.py
+++ dfCreation.py
@@ -6,8 +6,9 @@ Created on Fri Jul 28 14:14:28 2017
 """
 
 import numpy as np
-import pandas as pd
-x = np.random.randn(500)
-y = np.sin(x)
-df = pd.DataFrame({'x':x, 'y':y})
-df.plot('x', 'y', kind='scatter')
+from pandas import read_hdf
+# this query selects the columns A and B# where the values of A is greather than 0.5
+hdf = read_hdf('C:\\Users\\Aniket Pant\\Downloads\\inhibitors\\cdk2.h5')
+
+# x = pd.read_hdf("C:\\Users\\Aniket Pant\\Downloads\\inhibitors\\cdk2.h5")
+# x = HDFStore('C:\\Users\\Aniket Pant\\Downloads\\inhibitors\\cdk2.h5')
